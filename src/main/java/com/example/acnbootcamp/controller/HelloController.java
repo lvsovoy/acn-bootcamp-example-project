638dbe4ff873684a705800e651dd871c3f1c1cb5
Konstantins Lesovojs <konstantins.lesovojs@accenture.com>
2026-04-17 15:23:05 +0300

    added mechanism to test logs

--- src/main/java/com/example/acnbootcamp/controller/HelloController.java
+++ src/main/java/com/example/acnbootcamp/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.acnbootcamp.controller;
 
 import com.example.acnbootcamp.model.DeploymentInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Value("${app.deployment.branch:unknown}")
     private String deploymentBranch;
@@ -27,6 +33,12 @@ public class HelloController {
         );
         model.addAttribute("deploymentInfo", deploymentInfo);
         return "index";
+    }
+
+    @PostMapping("/message")
+    public String submitMessage(@RequestParam("message") String message) {
+        logger.info("User submitted message: {}", message);
+        return "redirect:/";
     }
 
 }
